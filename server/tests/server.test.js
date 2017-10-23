a8049375563501545be7af051383d3cb6d5af09c
KrisErdahl <>
2017-10-22 21:31:17 -0500

    add test cases for delete route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -112,3 +112,43 @@ describe('GET /todos/:id', () => {
 			.end(done);
 	});
 });
+
+describe('DELETE /todos/:id', () => {
+	it('should remove a todo', done => {
+		var hexId = todos[1]._id.toHexString();
+
+		request(app)
+			.delete(`/todos/${hexId}`)
+			.expect(200)
+			.expect(res => {
+				expect(res.body.todo._id).toBe(hexId);
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+				Todo.findById(hexId)
+					.then(todo => {
+						expect(todo).toNotExist();
+						done();
+					})
+					.catch(e => done(e));
+			});
+	});
+
+	it('should return 404 if todo not found', done => {
+		request(app)
+			.delete(`/todos/${ObjectID().toHexString()}`)
+			.expect(404)
+			.end(done);
+	});
+
+	it('should return 404 if object id is invalid', done => {
+		request(app)
+			//id=123 is invalid
+			.delete(`/todos/123`)
+			//${todos[1]._id.toHexString()}
+			.expect(404)
+			.end(done);
+	});
+});
